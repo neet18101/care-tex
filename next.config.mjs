e0602e1560a7ce2a966fe6eca764da7070f16476
neet18101 <neet18101@gmail.com>
2025-02-19 22:23:17 +0530

    fixing build

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  pageExtensions: ['js', 'jsx'], // Uncommented
+ 
 };
 
 export default nextConfig;
